13d89bf949876e2914cbb3c85de8ec3e8a54618f
kaisayy <sha0hk.m@gmail.com>
2018-06-25 16:40:24 +0900

    a

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ error_log("a");
     $api_key = getenv('docomoAPIKey');
     $api_url1 = sprintf('https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/registration?APIKEY=%s', $api_key);
     $req_body1 = array('botid' => "Chatting",
-                      'appkind' => "Smart Phone"
+                      'appKind' => "Smart Phone"
                       );
 
     $headers1 = array(  
